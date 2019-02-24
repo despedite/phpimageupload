a6bfdaf603d47703c9e00c956ec5f0b8c092fe9f
Erik Bianco Vera <36340014+despediteerik@users.noreply.github.com>
2019-02-23 21:45:08 -0300

    Translated error lines

--- upload-1.php
+++ upload-1.php
@@ -21,25 +21,25 @@ if(isset($_POST["submit"])) {
         echo "";
         $uploadOk = 1;
     } else {
-        echo "El archivo no es una imagen. ";
+        echo "This file isn't an image. ";
         $uploadOk = 0;
     }
 }
 
 // Check file size
 if ($_FILES["fileToUpload"]["size"] > 500000) {
-    echo "Tu imagen es demasiado grande! ";
+    echo "This image is too big! ";
     $uploadOk = 0;
 }
 // Allow certain file formats
 if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg"
 && $imageFileType != "gif" ) {
-    echo "Solo admitimos imagenes .JPG, .PNG, .JPEG y .GIF. ";
+    echo "Sorry! We only admit .JPG, .PNG, .JPEG and .GIF file formats. ";
     $uploadOk = 0;
 }
 // Check if $uploadOk is set to 0 by an error
 if ($uploadOk == 0) {
-    echo "Lo siento, tu archivo no pudo guardarse. ";
+    echo "Sorry, we couldn't save your file. ";
 // if everything is ok, try to upload file
 } else {
     if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
@@ -47,7 +47,7 @@ if ($uploadOk == 0) {
         //Replace INSERT-WEBSITE-LINK-HERE by your site's path. I.e.: http://img.erikbianco.me/i/
         Redirect('INSERT-WEBSITE-LINK-HERE/i/'.$_FILES["fileToUpload"]["name"], false);
     } else {
-        echo "Hubo un error subiendo tu archivo. Intentalo de nuevo! ";
+        echo "There was an error while trying to upload your file. Please, try again! ";
     }
 }
 ?>
